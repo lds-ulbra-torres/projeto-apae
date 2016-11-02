eb5a3a6567336031b5bd7ec466da17c03ed5e60f
Antoni <antonisganzerla@gmail.com>
2016-11-02 09:59:24 -0200

    atualizado scripts do banco

--- docs/bankSQL.sql
+++ docs/bankSQL.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Out-2016 às 16:45
+-- Generation Time: 02-Nov-2016 às 11:51
 -- Versão do servidor: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -29,16 +29,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `banks` (
   `id_bank` int(20) NOT NULL,
   `name_bank` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `phone_bank` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+  `phone_bank` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `agency_number` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `check_digit_agency` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `account_number` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `check_digit_account` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `banks`
 --
 
-INSERT INTO `banks` (`id_bank`, `name_bank`, `phone_bank`) VALUES
-(34, 'Banrisul   ', '(51) 3626-3222'),
-(37, 'Sicoob    ', '(51) 3626-2099');
+INSERT INTO `banks` (`id_bank`, `name_bank`, `phone_bank`, `agency_number`, `check_digit_agency`, `account_number`, `check_digit_account`) VALUES
+(34, 'Banrisul     ', '(51) 3626-3222', ' 234 ', '2', ' 23423 ', '5'),
+(37, 'Sicoob         ', '(51) 3626-2099', ' 3453 ', '5', '23454 ', '4'),
+(48, 'Banco do Brasil ', '(41) 2312-3213', '0578  ', '5  ', '123454  ', ' 8 ');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +63,7 @@ ALTER TABLE `banks`
 -- AUTO_INCREMENT for table `banks`
 --
 ALTER TABLE `banks`
-  MODIFY `id_bank` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_bank` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
